87381f2697702f60d4907becfac4b38589451dba
JH <jhk3261@gnu.ac.kr>
2025-02-27 20:43:03 +0900

    ✨ feat : JjinFilter 버튼 스크롤 구현

--- src/components/map/JjinFilterModal.tsx
+++ src/components/map/JjinFilterModal.tsx
@@ -2,17 +2,50 @@ import { useRecoilState } from "recoil";
 import { JjinFilterState } from "../../recoil/util/filterRecoilState";
 import styles from "./JjinFilterModal.module.css";
 import iconDown from "../../assets/image/iconDown.svg"
+import iconUp from "../../assets/image/iconUp.svg"
+import { useRef, useState } from "react";
 
 const JjinFilterModal = () => {
     const [filters, setFilters] = useRecoilState(JjinFilterState);
     
+    // 스크롤 상태 관리
+    const [scrolled, setScrolled] = useState<{[key:number]:Boolean}>({});
+    const filterContentRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+    // 아래로 스크롤 (한 번에 250px 이동)
+    const handleScrollDown = (index: number) => {
+        const ref = filterContentRefs.current[index];
+        if (ref) {
+            ref.scrollBy({ top: 250, behavior: "smooth" });
+            // 스크롤이 내려가면 이미지를 위로 스크롤로 바꿔줍니다.
+            setScrolled((prevState) => ({
+                ...prevState,
+                [index]: true,
+            }));
+        }
+    };
+
+    // 위로 스크롤
+    const handleScrollUp = (index : number) => {
+        const ref = filterContentRefs.current[index];
+        if (ref) {
+            ref.scrollBy({ top: -250, behavior: "smooth" });
+             // 스크롤이 내려가면 이미지를 위로 스크롤로 바꿔줍니다.
+             setScrolled((prevState) => ({
+                ...prevState,
+                [index]: false,
+            }));
+        }
+    };
+
     return (
         <div className={styles.content}>
             <div className={styles.filter_wrap}>
-                {filters.map((category) => (
+                {filters.map((category, index) => (
                     <div className={styles.jjin_filter_wrap}>
                         <p className={styles.title}>{category.category}</p>
-                        <div className={styles.jjin_filter}>
+                        <div className={styles.jjin_filter} 
+                        ref={(el: HTMLDivElement | null) => {filterContentRefs.current[index] = el;}}>
                             {category.filter.map((item, index) => (
                                 <button key={index} className={styles.filter_btn}>
                                     <img src={item.icon} alt={item.label} className={styles.filter_icon}/>
@@ -20,8 +53,15 @@ const JjinFilterModal = () => {
                                 </button>
                             ))}
                         </div>
-                        <button className={styles.bottom_btn}>
-                            <img src={iconDown} alt="스크롤"/>
+                        <button className={styles.bottom_btn} 
+                        onClick={() => {
+                            if (scrolled[index]) {
+                                handleScrollUp(index);  // 위로 스크롤
+                            } else {
+                                handleScrollDown(index); // 아래로 스크롤
+                            }
+                        }}>
+                            <img src={scrolled[index] ? iconUp : iconDown} alt="스크롤"/>
                         </button>
                     </div>
                 ))}
