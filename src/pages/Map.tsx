e18439ca682e18afe5899c89868f313c893ca4cf
JH <jhk3261@gnu.ac.kr>
2025-02-27 19:49:26 +0900

    🐛 bug : 모바일 바텀시트 오류 수정

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -6,9 +6,24 @@ import styles from "./Map.module.css";
 import FilterBar from '../components/map/FilterBar';
 
 const Map = () => {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(window.visualViewport?.height || window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+        
+        // 초기 로드 시 한 번 실행
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
-        <div className="content" >
+        <div className={styles.content}             
+            style={{ minHeight: `${windowHeight}px`, display: "flex", flexDirection: "column" }}>
             <div className={`${styles.container} ${styles.header_bar}`}>
                 <HousingFilter/>
                 <SearchBar/>
