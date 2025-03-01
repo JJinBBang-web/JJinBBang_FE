7403f51c5a3d41ea6157c694923aedf50b51051b
suin <btj1011@naver.com>
2025-03-01 22:49:30 +0900

    📦 chore : 필터 아이콘 수정

--- src/recoil/util/filterRecoilState.ts
+++ src/recoil/util/filterRecoilState.ts
@@ -75,8 +75,8 @@ export const JjinFilterState = atom<FilterCategory[]>({
       filter: [
         { label: '주차가 편리해요', icon: PO_MT_01 },
         { label: '엘리베이터가 있어요', icon: PO_MT_02 },
-        { label: '관리비가 합리적이에요', icon: PO_MT_03 },
-        { label: '쓰레기 처리가 편해요', icon: PO_MT_04 },
+        { label: '관리비가 합리적이에요', icon: PO_MT_04 },
+        { label: '쓰레기 처리가 편해요', icon: PO_MT_03 },
         { label: '인터넷이 잘 돼요', icon: PO_MT_05 },
         { label: '관리가 정기적이에요', icon: PO_MT_06 },
         { label: '이사가 편했어요', icon: PO_MT_07 },
