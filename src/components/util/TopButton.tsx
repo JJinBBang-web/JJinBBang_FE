6dde1692583d9c8d5c5a6962a64606fce41f6f73
JH <jhk3261@gnu.ac.kr>
2025-03-03 23:59:07 +0900

    ✨ feat : TopBtn 기능 구현

--- src/components/util/TopButton.tsx
+++ src/components/util/TopButton.tsx
@@ -2,8 +2,16 @@ import styles from "./TopButton.module.css"
 import topIcon from "../../assets/image/iconUp.svg"
 
 const TopButton:React.FC = () => {
+    const handleScrollTop = () => {
+        console.log("버튼 클릭")
+        document.documentElement.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
-        <div className={`${styles.content} ${styles.topBtn}`}>
+        <div className={`${styles.content} ${styles.topBtn}`} onClick={handleScrollTop}> 
             <img src={topIcon} alt="topBtn" style={{width:"28px"}}/>
         </div>
     )
