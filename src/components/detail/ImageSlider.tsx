d726c46cf5c1c27ffe4ad9dc62ce52db0844626d
JH <jhk3261@gnu.ac.kr>
2025-03-06 00:30:37 +0900

    ✨ feat : ImageSlider 이미지 개수 컴포넌트 구현

--- src/components/detail/ImageSlider.tsx
+++ src/components/detail/ImageSlider.tsx
@@ -1,9 +1,14 @@
 import styles from "./ImageSlider.module.css"
+import imgIcon from "../../assets/image/imageIcon.svg"
 
 const ImageSlider:React.FC = () => {
     return (
         <div className={styles.content}>
             {/* 슬라이더만들기 */}
+            <div className={styles.imgCount} >
+                <img src={imgIcon} alt="imgIcon" style={{width:"18px", height:"18px"}}/>
+                <p className={styles.count}>20</p>
+            </div>
         </div>
     )
 }
