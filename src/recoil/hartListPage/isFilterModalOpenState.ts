88df6b590d7ac5299823287e04937fb9f48c6a5d
이루경 <dlfnrud0605@gmail.com>
2025-05-16 13:42:57 +0900

    ♻️ refactor : 좋아요 수 연동, 모달 키 이름 변경

--- src/recoil/hartListPage/isFilterModalOpenState.ts
+++ src/recoil/hartListPage/isFilterModalOpenState.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
 export const isFilterModalOpenState = atom<boolean>({
-    key: "isSheetOpenState",
-    default: false,
-  });
+  key: "isFilterModalOpenState",
+  default: false,
+});
