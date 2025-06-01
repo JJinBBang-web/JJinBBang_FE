bb13d574de1b8fa93b79d7289af53eb2d651af49
JH <jhk3261@gnu.ac.kr>
2025-06-02 00:16:53 +0900

    ✨ feat : 후기 상세 UI 전체 구현

--- src/components/detail/ReviewMapInfo.tsx
+++ src/components/detail/ReviewMapInfo.tsx
@@ -14,14 +14,33 @@ interface Props {
 }
 
 const ReviewMapInfo:React.FC<Props> = ({review}) => {
+    const floor = review.generalReviewInfo?.floor ?? review.domitoryReviewInfo?.floor;
     return (
         <div className={styles.content}>
-            <p className={styles.title}>단지 정보</p>
-            <div className={styles.infoWrap}>
-                <p className={styles.subTitle}>유형</p>
-                <p className={styles.infoContent}>{review.building.type}</p>
-            </div>
-            <hr/>
+            {review.generalReviewInfo && (
+                <>
+                    <p className={styles.title}>단지 정보</p>
+                    <div className={styles.infoWrap}>
+                        <p className={styles.subTitle}>유형</p>
+                        <p className={styles.infoContent}>{review.building.type}</p>
+                    </div>
+                    <hr/>
+                </>
+            )}
+            {review.domitoryReviewInfo && (
+                <>
+                    <p className={styles.title}>단지 정보</p>
+                    <div className={styles.infoWrap}>
+                        <p className={styles.subTitle}>유형</p>
+                        <p className={styles.infoContent}>{review.building.type}</p>
+                    </div>
+                    <hr/>
+                </>
+                
+            )}
+            {review.agencyReviewInfo && (
+                <p className={styles.title}>공인중개사 정보</p>
+            )}
             <div className={styles.infoWrap}>
                 <p className={styles.subTitle}>주소</p>
                 <p className={styles.infoContent}>
@@ -33,7 +52,7 @@ const ReviewMapInfo:React.FC<Props> = ({review}) => {
                 <p className={styles.subTitle}>상세주소</p>
                 <p className={styles.infoContent}>
                     {review.building.name}<br/>
-                    {review.basicInfo.floor}           
+                    {floor}           
                 </p>
             </div>
             <div className={styles.map}>
@@ -42,7 +61,7 @@ const ReviewMapInfo:React.FC<Props> = ({review}) => {
                     lat: review.building.latitude,
                     lng: review.building.longitude
                     }}
-                    style={{ width: '100%', height: '100%', zIndex:"0"}}
+                    style={{ width: '100%', height: '100%', zIndex:"0", borderRadius:"12px"}}
                     level={3} // 줌 레벨 설정
                     draggable={false} // 드래그 비활성화
                     zoomable={false} // 확대/축소 비활성화
