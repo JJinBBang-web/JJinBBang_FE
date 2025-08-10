d4e8ed2ffc3a5b68d931263275deb758a8b8d528
이루경 <dlfnrud0605@gmail.com>
2025-08-10 22:01:57 +0900

    ✨ feat : reviewList 로컬스토리지 저장

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -1,110 +1,127 @@
-import { useEffect, useState } from 'react';
-import styles from './Review.module.css'
-import Header from '../components/Header';
-import ImageSlider from '../components/detail/ImageSlider';
-import TopButton from '../components/util/TopButton';
-import ReviewInfo from '../components/detail/ReviewInfo';
-import ReveiwContractInfo from '../components/detail/ReviewContractInfo';
-import ReviewMapInfo from '../components/detail/ReviewMapInfo';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { ReviewInfoState } from '../recoil/detail/ReviewInfoRecoliState';
-import Footer from '../components/detail/Footer';
-import ReportButton from '../components/util/ReportButton';
-import ReviewFacilitiesInfo from '../components/detail/ReviewFacilitiesInfo';
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
-import { useReviewDetail } from '../hooks/useReviewDetail';
-import { updateReviewState } from '../recoil/review/updateReviewAtoms';
-import { convertToReviewState } from '../util/convertToReviewState';
-
+import { useEffect, useState } from "react";
+import styles from "./Review.module.css";
+import Header from "../components/Header";
+import ImageSlider from "../components/detail/ImageSlider";
+import TopButton from "../components/util/TopButton";
+import ReviewInfo from "../components/detail/ReviewInfo";
+import ReveiwContractInfo from "../components/detail/ReviewContractInfo";
+import ReviewMapInfo from "../components/detail/ReviewMapInfo";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { ReviewInfoState } from "../recoil/detail/ReviewInfoRecoliState";
+import Footer from "../components/detail/Footer";
+import ReportButton from "../components/util/ReportButton";
+import ReviewFacilitiesInfo from "../components/detail/ReviewFacilitiesInfo";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useReviewDetail } from "../hooks/useReviewDetail";
+import { updateReviewState } from "../recoil/review/updateReviewAtoms";
+import { convertToReviewState } from "../util/convertToReviewState";
 
 const Review: React.FC = () => {
-    const navigate = useNavigate();
-    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-    const [reviews, setReviews] = useRecoilState(ReviewInfoState);
-    const setUpdateReview = useSetRecoilState(updateReviewState);
-    
-    const { reviewId } = useParams(); // /building/rv/:reviewId 형식이라면 필요
-    const [searchParams] = useSearchParams();
-    const reviewType = searchParams.get("reviewType") ?? "GENERAL"; // 쿼리에서 추출
+  const navigate = useNavigate();
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [reviews, setReviews] = useRecoilState(ReviewInfoState);
+  const setUpdateReview = useSetRecoilState(updateReviewState);
 
-    const { data, isLoading, isError } = useReviewDetail(reviewId ?? "", reviewType);
+  const { reviewId } = useParams(); // /building/rv/:reviewId 형식이라면 필요
+  const [searchParams] = useSearchParams();
+  const reviewType = searchParams.get("reviewType") ?? "GENERAL"; // 쿼리에서 추출
 
-    const loginUserId = 2;
+  const { data, isLoading, isError } = useReviewDetail(
+    reviewId ?? "",
+    reviewType
+  );
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowHeight(window.visualViewport?.height || window.innerHeight);
-        };
+  const loginUserId = 2;
 
-        window.addEventListener('resize', handleResize);
-        
-        // 초기 로드 시 한 번 실행
-        handleResize();
-
-        return () => window.removeEventListener('resize', handleResize);
-    })
-
-    useEffect(() => {
-        if (data) {
-        setReviews(data);
-        const converted = convertToReviewState(data);
-        console.log(converted);
-        setUpdateReview(converted);
-        localStorage.setItem('updateReviewState', JSON.stringify(converted));
-        }
-    }, [data]);
-        
-    const handleBack = () => {
-        navigate(-1);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.visualViewport?.height || window.innerHeight);
     };
 
-    if (isLoading) return <div>로딩 중...</div>;
-    if (isError || !data) return <div>리뷰 정보를 불러오지 못했습니다.</div>;
+    window.addEventListener("resize", handleResize);
+    const reviewList = JSON.parse(localStorage.getItem("reviewList") || "[]");
 
+    if (!reviewList || reviewList.length >= 5) {
+      reviewList.shift(); // 첫 번째 요소 제거
+    }
+    if (!reviewList.includes(Number(reviewId))) {
+      reviewList.push(Number(reviewId));
+    }
+    localStorage.setItem("reviewList", JSON.stringify(reviewList));
+    console.log("리뷰 리스트:", reviewList);
 
-    return (
-        <div className={styles.content}
-        style={{ minHeight: `${windowHeight}px`, display: "flex", flexDirection: "column" }}>
-            <div className={styles.container}>
-                {/* 헤더 */}
-                <Header onClick={handleBack}/>
-                {/* 이미지슬라이더 */}
-                <ImageSlider review={reviews} building={null}/>
-                {/* 리뷰 정보 및 키워드 */}
-                <ReviewInfo review={reviews}/>
-                <hr className={styles.divider} style={{marginTop:"50px"}}/>
-                {/* 계약형태 */}
-                {reviews.generalReviewInfo && (
-                    <>
-                    <ReveiwContractInfo review={reviews}/>
-                    <hr className={styles.divider}/>
-                    </>
-                )}
-                {reviews.domitoryReviewInfo && (
-                     <>
-                     <ReveiwContractInfo review={reviews}/>
-                     <hr className={styles.divider}/>
-                     <ReviewFacilitiesInfo review={reviews}/>
-                     <hr className={styles.divider}/>
-                     </>
-                )}
-                {/* 단지정보 */}
-                <ReviewMapInfo review={reviews}/>
-            </div>
-            {/* 작성id === 로그인 id 같으면 Footer 보이게+reportBtn안보이게, 아니면 반대 */}
-            { loginUserId == reviews.authorId ? 
-                <div className={styles.fixedWrap}>
-                    <TopButton/>
-                    <Footer reviewId={reviewId ?? ""} />
-                </div>
-                : 
-                <div className={styles.fixedWrap}>
-                    <ReportButton reviewId = {reviewId}/>
-                    <TopButton/>
-                </div>
-            }
+    // 초기 로드 시 한 번 실행
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  useEffect(() => {
+    if (data) {
+      setReviews(data);
+      const converted = convertToReviewState(data);
+      console.log(converted);
+      setUpdateReview(converted);
+      localStorage.setItem("updateReviewState", JSON.stringify(converted));
+    }
+  }, [data]);
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError || !data) return <div>리뷰 정보를 불러오지 못했습니다.</div>;
+
+  return (
+    <div
+      className={styles.content}
+      style={{
+        minHeight: `${windowHeight}px`,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div className={styles.container}>
+        {/* 헤더 */}
+        <Header onClick={handleBack} />
+        {/* 이미지슬라이더 */}
+        <ImageSlider review={reviews} building={null} />
+        {/* 리뷰 정보 및 키워드 */}
+        <ReviewInfo review={reviews} />
+        <hr className={styles.divider} style={{ marginTop: "50px" }} />
+        {/* 계약형태 */}
+        {reviews.generalReviewInfo && (
+          <>
+            <ReveiwContractInfo review={reviews} />
+            <hr className={styles.divider} />
+          </>
+        )}
+        {reviews.domitoryReviewInfo && (
+          <>
+            <ReveiwContractInfo review={reviews} />
+            <hr className={styles.divider} />
+            <ReviewFacilitiesInfo review={reviews} />
+            <hr className={styles.divider} />
+          </>
+        )}
+        {/* 단지정보 */}
+        <ReviewMapInfo review={reviews} />
+      </div>
+      {/* 작성id === 로그인 id 같으면 Footer 보이게+reportBtn안보이게, 아니면 반대 */}
+      {loginUserId == reviews.authorId ? (
+        <div className={styles.fixedWrap}>
+          <TopButton />
+          <Footer reviewId={reviewId ?? ""} />
         </div>
-    )
-}
+      ) : (
+        <div className={styles.fixedWrap}>
+          <ReportButton reviewId={reviewId} />
+          <TopButton />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Review;
