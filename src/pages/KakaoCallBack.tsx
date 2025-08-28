eadba73393be8fe8c8b447b76ddefc943d78b974
JH <jhk3261@gnu.ac.kr>
2025-08-28 21:35:50 +0900

    ✨ feat : 로그인 body 수정

--- src/pages/KakaoCallBack.tsx
+++ src/pages/KakaoCallBack.tsx
@@ -1,7 +1,7 @@
 // src/components/KakaoCallback.jsx
 
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { isLoginState } from '../recoil/auth/isLoginState';
 import { useRecoilState } from 'recoil';
@@ -10,6 +10,8 @@ import SignupCompleteModal from '../components/auth/SignupCompleteModal';
 
 // const url = process.env.REACT_APP_API_URL;
 // const url = '/api';
+const SITE_URL = process.env.REACT_APP_SITE_URL!;
+const loginUrl = `${SITE_URL}/login/kakao`;
 export const getSignupToken = () => localStorage.getItem('signupToken');
 export const getAccessToken = () => localStorage.getItem('accessToken');
 export const getRefreshToken = () => localStorage.getItem('refreshToken');
@@ -42,6 +44,7 @@ export const kakaoLogin = async (authCode: string) => {
     body: JSON.stringify({
       oauthProvider: 'kakao',
       oauthCode: authCode,
+      redirectUri: loginUrl,
     }),
   });
   return response.json();
