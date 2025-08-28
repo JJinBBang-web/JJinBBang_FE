b5313e9a7949e204e315843759400fb781fdcce9
JH <jhk3261@gnu.ac.kr>
2025-08-28 22:35:14 +0900

    ✨ feat : url 확인

--- src/pages/KakaoCallBack.tsx
+++ src/pages/KakaoCallBack.tsx
@@ -36,6 +36,15 @@ export const clearTokens = () => {
 };
 
 export const kakaoLogin = async (authCode: string) => {
+  const body = {
+    oauthProvider: 'kakao',
+    oauthCode: authCode,
+    redirectUri: loginUrl,
+  };
+
+  console.log("📦 Request Body:", body);               // 객체 그대로 출력
+  console.log("📦 Stringified Body:", JSON.stringify(body));
+  
   const response = await fetch('/api/v1/auth', {
     method: 'POST',
     headers: {
@@ -47,6 +56,7 @@ export const kakaoLogin = async (authCode: string) => {
       redirectUri: loginUrl,
     }),
   });
+  
   return response.json();
 };
 
