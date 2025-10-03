3ae43afcfbd84343d0cf35969a1bfe6daacbfd0c
JH <jhk3261@gnu.ac.kr>
2025-10-03 16:28:40 +0900

    🐛 fix : isLoginState 오류 해결

--- src/recoil/auth/isLoginState.ts
+++ src/recoil/auth/isLoginState.ts
@@ -1,7 +1,14 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 
 const { persistAtom } = recoilPersist({
   key: 'recoil-persist',
   storage: sessionStorage,
+});
+
+export const isLoginState = atom<boolean>({
+  key: "isLoginState",
+  default: false,
+  effects_UNSTABLE: [persistAtom],
 });
