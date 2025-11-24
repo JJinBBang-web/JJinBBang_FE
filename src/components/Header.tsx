aec1c84acd8988d84aa86ea19a6c868bc7646391
JH <jhk3261@gnu.ac.kr>
2025-11-24 20:23:30 +0900

    ♻️ refactor[#85] : 배너, 헤더, App.tsx 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,23 @@ import styles from "./Header.module.css"
 import backIcon from "../assets/image/backIcon.svg"
 
 interface HeaderProps {
+  type?: 'title' | 'icon';
+  title?: string;
   onClick?: () => void;
 }
 
-const Header:React.FC<HeaderProps> = ({onClick}) => {
+const Header:React.FC<HeaderProps> = ({type, onClick, title}) => {
     return (
         <div className={styles.content} onClick={onClick}>
             <img src={backIcon} alt="백버튼"/>
+            {type === 'title'
+            ? 
+            <>
+            <p className={styles.title}>{title}</p>
+                <div className={styles.nullPoint}/>
+            </>    
+            : null}
+            
         </div>
     )
 }
