573e91aac6786297ec523e95c1fc5865ef53b95e
JH <jhk3261@gnu.ac.kr>
2026-02-02 16:00:45 +0900

    ✨ feat[#129] 리뷰이벤트 팝업 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,19 +6,18 @@ import React from "react";
 import styles from "./Home.module.css";
 import home_logo from "../assets/logo/homeLogo.svg";
 import campus_icon from "../assets/image/campusIcon.svg";
-import Banner from "../components/Banner";
+// import Banner from "../components/Banner";
 import CampusSlide from "../components/CampusSlide";
 import PreviewReview from "../components/PreviewReview";
 import emptyCharacterIcon from "../assets/image/emptyCharacterIcon.svg";
-import pencil from "../assets/image/pencil.svg";
-import iconRight from "../assets/image/iconRight.svg";
+// import pencil from "../assets/image/pencil.svg";
+// import iconRight from "../assets/image/iconRight.svg";
 import { useQuery } from "@tanstack/react-query";
 import { getAPI } from "../api/baseAPI";
 import { isLoginState } from "../recoil/auth/isLoginState";
 import { useRecoilValue } from "recoil";
 import MetaTag from "../util/SEOMetaTag";
 import BannerCarousel from "../components/BannerCarousel";
-import PopupSheet from "../components/util/Popup";
 import ReviewEventBanner from "../assets/image/content/banner/ReviewEventbanner.png";
 import { useNavigate } from "react-router-dom";
 import { imageReloadVersionState } from "../recoil/util/imageReloadVersion";
@@ -293,7 +292,7 @@ const Home: React.FC = () => {
         )}
       </div>
       </div>
-      {/* <EventPopupSheet/> */}
+      <EventPopupSheet/>
     </>
   );
 };
